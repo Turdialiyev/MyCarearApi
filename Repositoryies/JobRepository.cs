53be2e17ef7ccf79877e0d9442d4702090a57b0a
Sultonxon <91827090+Sultonxon@users.noreply.github.com>
2022-12-07 16:35:11 +0500

    Adding Repositories

---
+++ Repositoryies/JobRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyCarearApi.Repositoryies.Interfaces;
+using MyCareerApi.Entities;
+using MyCarrearApi.Data;
+using MyCarrearApi.Repositories;
+
+namespace MyCarearApi.Repositoryies
+{
+    public class JobRepository: GenericRepository<Job>, IJobRepository
+    {
+        public JobRepository(AppDbContext appDbContext) : base(appDbContext) { }
+    }
+}
---
+++ Repositoryies/JobSkillsRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyCarearApi.Repositoryies.Interfaces;
+using MyCareerApi.Entities;
+using MyCarrearApi.Data;
+using MyCarrearApi.Repositories;
+
+namespace MyCarearApi.Repositoryies
+{
+    public class JobSkillsRepository: GenericRepository<JobSkills>, IJobSkillsRepository
+    {
+        public JobSkillsRepository(AppDbContext appDbContext) : base(appDbContext) { }
+    }
+}
