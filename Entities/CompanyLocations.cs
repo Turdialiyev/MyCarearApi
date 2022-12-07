b6fdbe5c7bc5285041f97f6de306b9bc5f0f2fb3
Behzod <behzodsafarov73@gmail.com>
2022-12-07 13:28:00 +0500

    Added My tasks

--- Entities/CompanyLocations.cs
+++ Entities/CompanyLocations.cs
@@ -1,3 +1,11 @@
 ﻿namespace MyCareerApi.Entities;
 
-public class CompanyLocations { }
+public class CompanyLocations
+ { 
+   public int Id { get; set; }
+   public string? Location { get; set; }
+
+   public int CompanyId { get; set; }
+   public Company Company { get; set; }
+ }
+
--- Entities/UserLanguages.cs
+++ Entities/UserLanguages.cs
@@ -1,2 +1,10 @@
 ﻿namespace MyCareerApi.Entities;
-public class UserLanguages { }
+public class UserLanguages 
+{ 
+    public int Id { get; set; }
+    public int Language { get; set; }
+    public Level Level {get; set;}
+    
+    public int FrelanceInfoId {get; set;}
+    public FrenaceInfo FrenaceInfo {get; set;}
+}
