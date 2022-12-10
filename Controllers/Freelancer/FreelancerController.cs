638e6df562dd10f79fd99e35c00aa1967805b364
Turdialiyev <turdialiyevgolibjon0226@gmail.com>
2022-12-10 14:35:29 +0500

    added Freelancer service in program.cs

---
+++ Controllers/Freelancer/FreelancerController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCarearApi.Controllers;
+
+[ApiController]
+[Route("api/[controlller]")]
+
+public class FreelancerController : ControllerBase
+{
+    // public FreelancerController(ILogger<FreelancerController> logger, )
+
+}
