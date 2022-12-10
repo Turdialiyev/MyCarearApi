04d69febfb4ca1726119dd6fd6a140cdb38a57dd
Turdialiyev <turdialiyevgolibjon0226@gmail.com>
2022-12-10 12:44:57 +0500

    added FreelancseHobby

--- Dtos/Freelancer/FreelancerPosition.cs
+++ Dtos/Freelancer/FreelancerPosition.cs
@@ -5,5 +5,6 @@ public class Position
     public string? Description { get; set; }
     public DateOnly Birthday { get; set; }
     public int PositionId { get; set; }
-    
+    public List<FreelancerHobby>? FreelancerHobbies{ get; set; }
+    public List<FreelancerSkill>? FreelancerSkills { get; set; }
 }
