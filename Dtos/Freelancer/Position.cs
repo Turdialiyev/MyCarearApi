732fe1b1fae80bea15248b7eeb12b200e452615d
Turdialiyev <turdialiyevgolibjon0226@gmail.com>
2022-12-10 08:55:22 +0500

    added Position Dto

---
+++ Dtos/Freelancer/Position.cs
@@ -0,0 +1,9 @@
+namespace MyCarearApi.Dtos;
+
+public class Position
+{
+    public string? Description { get; set; }
+    public DateOnly Birthday { get; set; }
+    public int PositionId { get; set; }
+    
+}
