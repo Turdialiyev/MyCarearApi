ee123250d96597dd73454b085d7e817a6039fbbe
Turdialiyev <turdialiyevgolibjon0226@gmail.com>
2022-12-10 13:42:56 +0500

    added Language dtoy

---
+++ Dtos/Freelancer/FreelancerLanguage.cs
@@ -0,0 +1,9 @@
+using MyCarearApi.Entities.Enums;
+
+namespace MyCarearApi.Dtos;
+
+public class FreelancerLanguage
+{
+    public int LanguageId  { get; set; }
+    public LangLevel Level {get; set;}
+}
